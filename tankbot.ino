facf24720835e844a41635022481aa17df356506
romelectr0 <romelectr0@users.noreply.github.com>
2015-10-23 22:46:12 +0200

    Update tankbot.ino

--- tankbot.ino
+++ tankbot.ino
@@ -1,8 +1,6 @@
 #include <AFMotor.h> 
-#include <Servo.h>
 #include <math.h>
 AF_Stepper stepper(200,2);
-Servo servo;
 AF_DCMotor motor2(2, MOTOR12_8KHZ); 
 AF_DCMotor motor(1, MOTOR12_8KHZ);
 float pi = 3.14159265359;
@@ -13,6 +11,8 @@ double cosangularecart = cos(angularecart);
 long ecartmax = cosangularecart*deplacementspeed;
 int trigpin = A0;
 int echopin = A1;
+int ecopinleft = A2;
+int echopinright = A3;
 long timer;
 long dist[2];
 boolean trig = true;
@@ -23,7 +23,6 @@ void setup() {
   pinMode(A0,OUTPUT);
   digitalWrite(trigpin,LOW);
   pinMode(A1,INPUT);
-  servo.attach(A2);
   motor.setSpeed(motorspeed);   
   motor2.setSpeed(motorspeed);
   stepper.setSpeed(20);
@@ -31,7 +30,7 @@ void setup() {
  
 void loop() {
   //servo.write(90);
-  int timer = millis();
+  int timesince = millis();
   trig = !trig;
   digitalWrite(trigpin,HIGH);
   delayMicroseconds(10);
@@ -45,7 +44,7 @@ void loop() {
     motor2.run(BACKWARD);
     delay(1000);
     motor.run(FORWARD);
-    delay(1400);
+    delay(1300);
     motor.run(RELEASE);
     delay(10);
     look = true;
@@ -57,10 +56,10 @@ void loop() {
   else {
   motor.run(FORWARD);
   motor2.run(FORWARD);
-  if (look && timer > 10000) {
+  if (look && timesince > 10000) {
     stepper.step(100,BACKWARD,SINGLE);
     stepper.release();
-    timer = 0;
+    timesince = 0;
   }
   look = false;
   //stayparralel(trig);
@@ -68,21 +67,21 @@ void loop() {
 }
 
 void stayparralel (boolean side) {
-  if (side) angle = 0;
-  if (!side) angle = 180;
+  int pintoecho;
+  if (side) pintoecho = leftechopin;
+  if (!side) pintoecho = rightechopin;
   motor.run(FORWARD);  
   motor2.run(FORWARD);
-  servo.write(angle);
   digitalWrite(trigpin,HIGH);
   delayMicroseconds(10);
   digitalWrite(trigpin,LOW);
-  timer = pulseIn(echopin,HIGH);
+  timer = pulseIn(pintoecho,HIGH);
   dist[0] = timer/58;
   delay(500);
   digitalWrite(trigpin,HIGH);
   delayMicroseconds(10);
   digitalWrite(trigpin,LOW);
-  timer = pulseIn(echopin,HIGH);
+  timer = pulseIn(pintoecho,HIGH);
   dist[1] = timer/58;
   float timeforparralel = ((((pi/2)-acos((sqrt((dist[0]-dist[1])*(dist[0]-dist[1])))/deplacementspeed)))/deplacementspeed)*1000;
   int timeabs = fabs(timeforparralel);
@@ -96,13 +95,15 @@ void stayparralel (boolean side) {
 }
 void parralelfunction(boolean invert,boolean side,int timeabs) {
   boolean state;
+  int pintoecho;
+  if (side) pintoecho = leftechopin;
+  if (!side) pintoecho = rightechopin;
   if (!invert) state = side;
   else state = !side;
       motor.run(RELEASE); 
       motor2.run(RELEASE);
       if (state) {
-        motor.run(FORWARD);        
-
+        motor.run(FORWARD);
       }
       else {
         motor2.run(BACKWARD);
@@ -115,14 +116,14 @@ void parralelfunction(boolean invert,boolean side,int timeabs) {
       digitalWrite(trigpin,HIGH);
       delayMicroseconds(10);
       digitalWrite(trigpin,LOW);
-      timer = pulseIn(echopin,HIGH);
+      timer = pulseIn(pintoecho,HIGH);
       dist[0] = timer/58;
       delay(500);
       digitalWrite(trigpin,HIGH);
       delayMicroseconds(10);
       digitalWrite(trigpin,LOW);
-      timer = pulseIn(echopin,HIGH);
-      dist[1] = timer/58;  
+      timer = pulseIn(pintoecho,HIGH);
+      dist[1] = timer/58;
 }
 void avoidside(boolean side) {
   if (dist[1] < 5) {
@@ -136,4 +137,42 @@ void avoidside(boolean side) {
       }
       delay(500);      
   }
+}
+void gotopoint(float x,float y) {;
+  float destination[2];
+  float polar[2];
+  int timetorelease;
+  int actualtime;
+  destination[0] = x;
+  destination[1] = y;
+  polar[0] = sqrt((destination[0]*destination[0])+(destination[1]*destination[1]));
+  polar[1] = acos(destination[0]/polar[0]);
+  robotangle(polar[1]);
+  timetorelease = robotgo(polar[1]);
+  actulatime = millis();
+  while(true) {
+  digitalWrite(trigpin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigpin,LOW);
+  timer = pulseIn(echopin,HIGH);
+  dist[0] = timer/58;
+  if (actualtime == timetorelease) {
+    motor.run(RELEASE);
+    motor2.run(RELEASE);
+    break;
+  }
+  if ((actualtime-1000 < timetorelease) && (dist[0] < 18)) {
+    polar[0] = actualtime*0.002*deplacementspeed;
+    destination[0] = destination[0]-(polar[0]*cos(polar[1]));
+    destination[1] = destination[1]-(polar[0]*sin(polar[1]));
+    robotangle(360-polar[1]);
+        
+  }
+ }
+ actualtime = 0;
+ 
+}
+int robotangle(float angletodo) {
+}
+void robotgo(float distancetotravel) {
 }
